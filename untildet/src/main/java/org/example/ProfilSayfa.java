2fa552237b9cec23df8e73b6308bb03454a24433
cebrail <cemg346807@gmail.com>
2025-02-07 18:52:09 +0300

    isim ekleedeil

--- untildet/src/main/java/org/example/ProfilSayfa.java
+++ untildet/src/main/java/org/example/ProfilSayfa.java
@@ -1,15 +1,22 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ProfilSayfa {
     JFrame frame = new JFrame();
+    JLabel label = new JLabel();
 
     public ProfilSayfa(){
+        label.setText("İsim: ŞİMŞEK MCQEEN");
+        label.setFont(new Font(null,Font.PLAIN,15));
+
+        frame.add(label);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle("Profil sayfa");
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout(3,1));
         frame.setVisible(true);
 
     }
