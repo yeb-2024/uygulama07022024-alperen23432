e6abe842069111e40b841092a31ab1921f3793a9
cebrail <cemg346807@gmail.com>
2025-02-07 18:35:44 +0300

    button eklendi

--- untildet/src/main/java/org/example/AnaSayfa.java
+++ untildet/src/main/java/org/example/AnaSayfa.java
@@ -1,16 +1,29 @@
 ﻿package org.example;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AnaSayfa {
+public class AnaSayfa implements ActionListener {
 
     JFrame frame = new JFrame();
-
+    JButton button = new JButton("Profili Göster");
     public AnaSayfa(){
+        button.addActionListener(this);
+        button.setBounds(200,200,100,50);
+        frame.add(button);
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle("AnaSayfa");
         frame.setLayout(null);
         frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+
     }
 }
